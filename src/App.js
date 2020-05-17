36406e0e4097537446f5b5a632ade1a9d6eb6097
sougato Pal <sougatopal1@gmail.com>
2020-05-17 19:25:04 +0530

    added pagination and styles

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 import React from "react";
 import "./styles.css";
 
+const api = "https://hn.algolia.com/api/v1/search?tags=front_page";
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      hits: []
+      hits: [],
+      curPages: 0,
+      totPage: 0
     };
+    this.paginationFn = this.paginationFn.bind(this);
+    this.fetchNews = this.fetchNews.bind(this);
+    this.urlTrimmer = this.urlTrimmer.bind(this);
   }
   componentDidMount() {
-    const api = "https://hn.algolia.com/api/v1/search?tags=front_page";
-    fetch(api)
+    this.fetchNews();
+  }
+  fetchNews(pageNo) {
+    let url = pageNo ? api + "&page=" + pageNo : api;
+    fetch(url)
       .then(res => res.json())
       .then(res => {
         console.log(res);
         let hits = res.hits;
-        this.setState({ hits });
+        let curPages = res.page;
+        let totPage = res.nbPages - 1;
+        this.setState({ hits, curPages, totPage });
       });
+  }
+  paginationFn(side) {
+    console.log(side);
+    if (side === "next") {
+      let curPage = this.state.curPages;
+      this.fetchNews(curPage + 1);
+    }
+    if (side === "prev") {
+      let curPage = this.state.curPages;
+      this.fetchNews(curPage - 1);
+    }
+  }
+  urlTrimmer(url) {
+    var urlParts = url
+      .replace("http://", "")
+      .replace("https://", "")
+      .split(/[/?#]/);
+    return urlParts[0];
   }
   render() {
     return (
@@ -33,13 +63,41 @@ export default class App extends React.Component {
               <div key={index} className="news-wrapper">
                 <div className="com-col">{elem.num_comments}</div>
                 <div className="vote-col">{elem.points}</div>
-                <div className="upvote-col">{elem.points}</div>
-                <div className="news-col">{elem.title}</div>
+                <div className="upvote-col" />
+                <div className="news-col">
+                  <span className="news-text">{elem.title} </span>
+                  <a href={elem.url} rel="noopener noreferrer" target="_blank">
+                    {this.urlTrimmer(elem.url)}
+                  </a>{" "}
+                  by <span className="author-text">{elem.author}</span>
+                </div>
               </div>
             );
           })}
         </div>
-        <footer className="footer">pagination</footer>
+        <footer className="footer">
+          {this.state.curPages !== 0 ? (
+            <button
+              className="pagination-bt"
+              onClick={e => this.paginationFn("prev")}
+            >
+              prev
+            </button>
+          ) : (
+            ""
+          )}
+          |
+          {this.state.curPages !== this.state.totPage ? (
+            <button
+              className="pagination-bt"
+              onClick={e => this.paginationFn("next")}
+            >
+              next
+            </button>
+          ) : (
+            ""
+          )}
+        </footer>
       </div>
     );
   }
