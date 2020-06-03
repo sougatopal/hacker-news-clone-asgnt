d866b5511f425c7c00eaee6cc431453dae9159bb
sougato Pal <sougatopal1@gmail.com>
2020-06-04 01:27:51 +0530

    moved footer to footer component

--- src/components/Linegraph.js
+++ src/components/Linegraph.js
@@ -12,37 +12,35 @@ import {
   Tooltip
 } from "devextreme-react/ui/chart";
 
-class Linegraph extends React.Component {
-  customizeTooltip(arg) {
-    return {
-      text: arg.valueText
-    };
-  }
-  render() {
-    return (
-      <div id={"chart-demo"}>
-        <Chart palette={"Violet"} dataSource={this.props.linegraphData}>
-          <CommonSeriesSettings argumentField={"ID"} type={"line"} />
-          <Series key={"vote"} valueField={"vote"} name={"Vote"} />
-          <Margin bottom={20} />
-          <ArgumentAxis
-            valueMarginsEnabled={false}
-            discreteAxisDivisionMode={"crossLabels"}
-          >
-            <Grid visible={true} />
-          </ArgumentAxis>
-          <Legend
-            verticalAlignment={"bottom"}
-            horizontalAlignment={"center"}
-            itemTextPosition={"bottom"}
-          />
-          <Export enabled={true} />
-          <Title text={"votes vs ID"} />
-          <Tooltip enabled={true} customizeTooltip={this.customizeTooltip} />
-        </Chart>
-      </div>
-    );
-  }
+function customizeTooltip(arg) {
+  return {
+    text: arg.valueText
+  };
+}
+function Linegraph(props) {
+  return (
+    <div id={"chart-demo"}>
+      <Chart palette={"Violet"} dataSource={props.linegraphData}>
+        <CommonSeriesSettings argumentField={"ID"} type={"line"} />
+        <Series key={"vote"} valueField={"vote"} name={"Vote"} />
+        <Margin bottom={20} />
+        <ArgumentAxis
+          valueMarginsEnabled={false}
+          discreteAxisDivisionMode={"crossLabels"}
+        >
+          <Grid visible={true} />
+        </ArgumentAxis>
+        <Legend
+          verticalAlignment={"bottom"}
+          horizontalAlignment={"center"}
+          itemTextPosition={"bottom"}
+        />
+        <Export enabled={true} />
+        <Title text={"votes vs ID"} />
+        <Tooltip enabled={true} customizeTooltip={customizeTooltip} />
+      </Chart>
+    </div>
+  );
 }
 
 export default Linegraph;
